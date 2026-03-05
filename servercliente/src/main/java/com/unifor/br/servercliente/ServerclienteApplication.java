e7baa2945fd7de8f2b9f7805041205d821a8d572
Marcos <marcosaurelioce@hotmail.com>
2026-03-04 23:16:20 -0300

    initial commit

---
+++ servercliente/src/main/java/com/unifor/br/servercliente/ServerclienteApplication.java
@@ -0,0 +1,31 @@
+package com.unifor.br.servercliente;
+
+import com.unifor.br.servercliente.model.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ServerclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerclienteApplication.class, args);
+
+		String url = "http://localhost:8080/users";
+
+		RestTemplate restTemplate = new RestTemplate();
+
+//		String json = """
+//				{
+//				"id": 1,
+//				"name": "Teste",
+//				"email": "teste@hotmail.com"
+//				}""";
+
+		User usuario = new User(1L,"teste", "teste@hotmail.com");
+		restTemplate.postForObject(url, usuario, String.class);
+		System.out.println("Usuario enviado com sucesso!");
+	}
+
+}
+
